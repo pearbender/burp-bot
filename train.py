4e990e05ac2974337571c2e954bc6d00aa90435f
pearbender <pearbender@gmail.com>
2023-07-25 05:24:21 +0900

    Create train.py

---
+++ train.py
@@ -0,0 +1,220 @@
+from torch.utils.data import DataLoader, Dataset, random_split
+import torch
+import torchaudio
+from torchaudio import transforms
+import os
+import torch.nn.functional as F
+from torch.nn import init
+from torch import nn
+
+
+class SoundDS(Dataset):
+    def __init__(self, burps_files, not_burps_files):
+        self.burps_files = burps_files
+        self.not_burps_files = not_burps_files
+        self.duration = 30000
+        self.sr = 44100
+        self.channel = 2
+
+    def __len__(self):
+        return len(self.burps_files) + len(self.not_burps_files)
+
+    def __getitem__(self, idx):
+        if idx < len(self.burps_files):
+            audio_file = self.burps_files[idx]
+            class_id = 'burp'
+        else:
+            audio_file = self.not_burps_files[idx - len(self.burps_files)]
+            class_id = 'not-burp'
+
+        sig, sr = torchaudio.load(audio_file)
+        spec = transforms.MelSpectrogram(
+            sr, n_fft=1024, hop_length=None, n_mels=64)(sig)
+        spec = transforms.AmplitudeToDB(top_db=80)(spec)
+
+        return spec, class_id
+
+
+class AudioClassifier (nn.Module):
+    # ----------------------------
+    # Build the model architecture
+    # ----------------------------
+    def __init__(self):
+        super().__init__()
+        conv_layers = []
+
+        # First Convolution Block with Relu and Batch Norm. Use Kaiming Initialization
+        self.conv1 = nn.Conv2d(2, 8, kernel_size=(
+            5, 5), stride=(2, 2), padding=(2, 2))
+        self.relu1 = nn.ReLU()
+        self.bn1 = nn.BatchNorm2d(8)
+        init.kaiming_normal_(self.conv1.weight, a=0.1)
+        self.conv1.bias.data.zero_()
+        conv_layers += [self.conv1, self.relu1, self.bn1]
+
+        # Second Convolution Block
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=(
+            3, 3), stride=(2, 2), padding=(1, 1))
+        self.relu2 = nn.ReLU()
+        self.bn2 = nn.BatchNorm2d(16)
+        init.kaiming_normal_(self.conv2.weight, a=0.1)
+        self.conv2.bias.data.zero_()
+        conv_layers += [self.conv2, self.relu2, self.bn2]
+
+        # Second Convolution Block
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=(
+            3, 3), stride=(2, 2), padding=(1, 1))
+        self.relu3 = nn.ReLU()
+        self.bn3 = nn.BatchNorm2d(32)
+        init.kaiming_normal_(self.conv3.weight, a=0.1)
+        self.conv3.bias.data.zero_()
+        conv_layers += [self.conv3, self.relu3, self.bn3]
+
+        # Second Convolution Block
+        self.conv4 = nn.Conv2d(32, 64, kernel_size=(
+            3, 3), stride=(2, 2), padding=(1, 1))
+        self.relu4 = nn.ReLU()
+        self.bn4 = nn.BatchNorm2d(64)
+        init.kaiming_normal_(self.conv4.weight, a=0.1)
+        self.conv4.bias.data.zero_()
+        conv_layers += [self.conv4, self.relu4, self.bn4]
+
+        # Linear Classifier
+        self.ap = nn.AdaptiveAvgPool2d(output_size=1)
+        self.lin = nn.Linear(in_features=64, out_features=10)
+
+        # Wrap the Convolutional Blocks
+        self.conv = nn.Sequential(*conv_layers)
+
+    # ----------------------------
+    # Forward pass computations
+    # ----------------------------
+    def forward(self, x):
+        # Run the convolutional blocks
+        x = self.conv(x)
+
+        # Adaptive pool and flatten for input to linear layer
+        x = self.ap(x)
+        x = x.view(x.shape[0], -1)
+
+        # Linear layer
+        x = self.lin(x)
+
+        # Final output
+        return x
+
+
+def training(model, train_dl, num_epochs):
+    # Loss Function, Optimizer and Scheduler
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.001,
+                                                    steps_per_epoch=int(
+                                                        len(train_dl)),
+                                                    epochs=num_epochs,
+                                                    anneal_strategy='linear')
+
+    # Repeat for each epoch
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        correct_prediction = 0
+        total_prediction = 0
+
+        # Repeat for each batch in the training set
+        for i, data in enumerate(train_dl):
+            # Get the input features and target labels, and put them on the GPU
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            # Normalize the inputs
+            inputs_m, inputs_s = inputs.mean(), inputs.std()
+            inputs = (inputs - inputs_m) / inputs_s
+
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            # Keep stats for Loss and Accuracy
+            running_loss += loss.item()
+
+            # Get the predicted class with the highest score
+            _, prediction = torch.max(outputs, 1)
+            # Count of predictions that matched the target label
+            correct_prediction += (prediction == labels).sum().item()
+            total_prediction += prediction.shape[0]
+
+            # if i % 10 == 0:    # print every 10 mini-batches
+            #    print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 10))
+
+        # Print stats at the end of the epoch
+        num_batches = len(train_dl)
+        avg_loss = running_loss / num_batches
+        acc = correct_prediction/total_prediction
+        print(f'Epoch: {epoch}, Loss: {avg_loss:.2f}, Accuracy: {acc:.2f}')
+
+    print('Finished Training')
+
+
+def inference(model, val_dl):
+    correct_prediction = 0
+    total_prediction = 0
+
+    # Disable gradient updates
+    with torch.no_grad():
+        for data in val_dl:
+            # Get the input features and target labels, and put them on the GPU
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            # Normalize the inputs
+            inputs_m, inputs_s = inputs.mean(), inputs.std()
+            inputs = (inputs - inputs_m) / inputs_s
+
+            # Get predictions
+            outputs = model(inputs)
+
+            # Get the predicted class with the highest score
+            _, prediction = torch.max(outputs, 1)
+            # Count of predictions that matched the target label
+            correct_prediction += (prediction == labels).sum().item()
+            total_prediction += prediction.shape[0]
+
+    acc = correct_prediction/total_prediction
+    print(f'Accuracy: {acc:.2f}, Total items: {total_prediction}')
+
+
+burps_folder_path = "./burps-audio"
+burps_files = [os.path.join(burps_folder_path, file) for file in os.listdir(
+    burps_folder_path) if file.lower().endswith(".wav")]
+
+burps_folder_path = "./not-burps-audio"
+not_burps_files = [os.path.join(burps_folder_path, file) for file in os.listdir(
+    burps_folder_path) if file.lower().endswith(".wav")]
+
+myds = SoundDS(burps_files, not_burps_files)
+num_items = len(myds)
+num_train = round(num_items * 0.8)
+num_val = num_items - num_train
+train_ds, val_ds = random_split(myds, [num_train, num_val])
+
+# Create training and validation data loaders
+train_dl = torch.utils.data.DataLoader(train_ds, batch_size=16, shuffle=True)
+val_dl = torch.utils.data.DataLoader(val_ds, batch_size=16, shuffle=False)
+
+# Create the model and put it on the GPU if available
+myModel = AudioClassifier()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+myModel = myModel.to(device)
+# Check that it is on Cuda
+next(myModel.parameters()).device
+
+num_epochs = 2   # Just for demo, adjust this higher.
+training(myModel, train_dl, num_epochs)
+print('Done')
+
+# Run inference on trained model with the validation set
+#inference(myModel, val_dl)
