ffb6e83eeafcec19a733ac38bd92e679acaf2228
laxadeR <alexlarex@yandex.ru>
2023-07-25 14:12:56 +0400

    demo eval script

--- eval.py
+++ eval.py
@@ -1,6 +1,55 @@
-from model import *
+from torch.utils.data import DataLoader, Dataset, random_split
 import torch
+import torchaudio
+from torchaudio import transforms
+import os
+import random
+import torch.nn.functional as F
+from torch.nn import init
+from torch import nn
+
+from model import *
+
 
 model = AudioClassifier()
 model.load_state_dict(torch.load("./model.pt"))
 model.eval()
+
+
+def prepare_file(audio_file):
+    sig, sr = torchaudio.load(audio_file)
+    spec = transforms.MelSpectrogram(
+        sr, n_fft=1024, hop_length=None, n_mels=64)(sig)
+    spec = transforms.AmplitudeToDB(top_db=80)(spec)
+
+    return spec.unsqueeze(0)
+
+
+def get_prediction(audio_file):
+    inputs = prepare_file(audio_file)
+    
+    inputs_m, inputs_s = inputs.mean(), inputs.std()
+    inputs = (inputs - inputs_m) / inputs_s
+    
+    output = model.forward(inputs)
+
+    conf, classes = torch.max(output, 1)
+
+    return conf.item(), classes.item()
+
+
+burps_folder_path = "./burps-audio"
+burps_files = [os.path.join(burps_folder_path, file) for file in os.listdir(
+    burps_folder_path) if file.lower().endswith(".wav")]
+
+burps_folder_path = "./not-burps-audio"
+not_burps_files = [os.path.join(burps_folder_path, file) for file in os.listdir(
+    burps_folder_path) if file.lower().endswith(".wav")]
+
+print("burps")
+for i in range(10):
+    print(get_prediction(random.choice(burps_files)))
+
+print("\n\n\nnon burps")
+for i in range(10):
+    print(get_prediction(random.choice(not_burps_files)))
